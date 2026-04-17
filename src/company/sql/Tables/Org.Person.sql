0d78d6ae39dc6829701a38a3541b42b9b381d396
Jay <52184578+Prokope45@users.noreply.github.com>
2026-04-17 03:54:28 +0000

    Add person table and seed data creation files.

---
+++ src/company/sql/Tables/Org.Person.sql
@@ -0,0 +1,15 @@
+IF OBJECT_ID(N'Org.Person') IS NULL
+BEGIN
+    CREATE TABLE Org.[Person] (
+        [PersonID] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+        [AddressID] INT NOT NULL,
+        [FirstName] NVARCHAR(100) NOT NULL,
+        [LastName] NVARCHAR(100) NOT NULL,
+        [Email] NVARCHAR(255) NOT NULL UNIQUE,
+        [Phone] NVARCHAR(50),
+        CONSTRAINT [FK_Person_Address] FOREIGN KEY ([AddressID])
+            REFERENCES Org.[Address]([AddressID])
+            ON DELETE CASCADE,
+        CONSTRAINT [CK_Person_Email_Length] CHECK (LEN([Email]) > 0)
+    );
+END;
