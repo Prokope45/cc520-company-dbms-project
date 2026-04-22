083f7154acf51bbe660a4f96d322309756f7c7fe
Jay <52184578+Prokope45@users.noreply.github.com>
2026-04-22 01:28:13 +0000

    Add missing MaxHoursPerWeek column to HourlyEmployee

--- src/company/sql/Tables/Org.HourlyEmployee.sql
+++ src/company/sql/Tables/Org.HourlyEmployee.sql
@@ -4,6 +4,7 @@ BEGIN
         [EmployeeID] INT NOT NULL PRIMARY KEY,
         [EmployeeTypeID] INT NOT NULL,
         [HourlyPay] DECIMAL(18, 2) NOT NULL,
+        [MaxHoursPerWeek] INT NULL,
         CONSTRAINT [FK_HourlyEmployee_Employee] FOREIGN KEY ([EmployeeID])
             REFERENCES Org.[Employee]([EmployeeID])
             ON DELETE CASCADE,
