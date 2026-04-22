4ff6f423be715ef33e3977d240d086635d569740
Jay <52184578+Prokope45@users.noreply.github.com>
2026-04-22 01:27:46 +0000

    Uncomment and test Employee table constraints

--- src/company/sql/Tables/Org.Employee.sql
+++ src/company/sql/Tables/Org.Employee.sql
@@ -8,27 +8,25 @@ BEGIN
         [EmployeeTypeID] INT NOT NULL,
         [HireDate] DATETIME2 NULL,
         [TerminationDate] DATETIME2 NULL,
-        -- CONSTRAINT [FK_Employee_Manager] FOREIGN KEY ([ManagerID])
-        --     REFERENCES [Employee]([EmployeeID])
-        --     ON DELETE SET NULL,
-        --     ON UPDATE CASCADE,
+        CONSTRAINT [FK_Employee_Manager] FOREIGN KEY ([ManagerID])
+            REFERENCES Org.[Employee]([EmployeeID]),
         CONSTRAINT [FK_Employee_Person] FOREIGN KEY ([PersonID])
             REFERENCES Org.[Person]([PersonID]),
         CONSTRAINT [FK_Employee_EmployeeType] FOREIGN KEY ([EmployeeTypeID])
-            REFERENCES Org.[EmployeeType]([EmployeeTypeID])
-        -- CONSTRAINT [UK_Employee_PersonID] UNIQUE ([PersonID]),
-        -- CONSTRAINT [UK_Employee_EmployeeID] UNIQUE ([EmployeeID]),
-        -- CONSTRAINT [CK_Employee_HireDate_NotNull_Salaried] CHECK (
-        --     ([EmployeeTypeID] = 1 AND [HireDate] IS NOT NULL) OR
-        --     ([EmployeeTypeID] = 2 AND [HireDate] IS NOT NULL) OR
-        --     ([HireDate] IS NULL)
-        -- ),
-        -- CONSTRAINT [CK_Employee_TerminationDate_NotNull] CHECK (
-        --     ([TerminationDate] IS NULL) OR
-        --     ([TerminationDate] > [HireDate])
-        -- ),
-        -- CONSTRAINT [CK_Employee_Manager_NotSelf] CHECK (
-        --     [ManagerID] IS NULL OR [ManagerID] <> [EmployeeID]
-        -- )
+            REFERENCES Org.[EmployeeType]([EmployeeTypeID]),
+        CONSTRAINT [UK_Employee_PersonID] UNIQUE ([PersonID]),
+        CONSTRAINT [UK_Employee_EmployeeID] UNIQUE ([EmployeeID]),
+        CONSTRAINT [CK_Employee_HireDate_NotNull_Salaried] CHECK (
+            ([EmployeeTypeID] = 1 AND [HireDate] IS NOT NULL) OR
+            ([EmployeeTypeID] = 2 AND [HireDate] IS NOT NULL) OR
+            ([HireDate] IS NULL)
+        ),
+        CONSTRAINT [CK_Employee_TerminationDate_NotNull] CHECK (
+            ([TerminationDate] IS NULL) OR
+            ([TerminationDate] > [HireDate])
+        ),
+        CONSTRAINT [CK_Employee_Manager_NotSelf] CHECK (
+            [ManagerID] IS NULL OR [ManagerID] <> [EmployeeID]
+        )
     );
 END;
