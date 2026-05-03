3f0d8cb1df157ec57e057e4527fd5bd076354aa2
Jay <52184578+Prokope45@users.noreply.github.com>
2026-05-03 17:45:29 +0000

    Update delete department SP: - Update delete department SP to remove DER relation mappings so that employees just lose their role and department values without being unintentionally deleted.

--- src/company/sql/Procedures/Department/DeleteDepartment.sql
+++ src/company/sql/Procedures/Department/DeleteDepartment.sql
@@ -2,7 +2,12 @@ CREATE PROCEDURE [Org].[sp_DeleteDepartment]
     @DepartmentID INT
 AS
 BEGIN
+    -- Before deleting department, delete DepEmpRole records for employees in department.
+    -- This leaves employees as "floating" without a department or role.
+    DELETE FROM [Org].[DepEmpRole]
+    WHERE DepartmentID = @DepartmentID;
+
     DELETE FROM [Org].[Department]
-    WHERE DepartmentID = @DepartmentID
+    WHERE DepartmentID = @DepartmentID;
 END
 GO
