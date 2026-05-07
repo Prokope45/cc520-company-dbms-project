c10f4256518fd260462b81df0aec3a68102197f2
Jay <52184578+Prokope45@users.noreply.github.com>
2026-05-07 03:35:24 +0000

    Replace hightest CEO stored procedures with highest paid executives

--- src/company/sql/Procedures/Reports/Report_HighestPaidExecutive.sql
+++ src/company/sql/Procedures/Reports/Report_HighestPaidExecutive.sql
@@ -1,19 +1,20 @@
-CREATE PROCEDURE [Org].[sp_Report_HighestPaidCEO_Aggregated]
+CREATE PROCEDURE [Org].[sp_Report_HighestPaidExecutive]
 AS
 BEGIN
-    SELECT
+    SELECT TOP(1)
         C.[Name] AS CompanyName,
-        COUNT(*) AS CEOCount,
-        MAX(S.BaseSalary) AS HighestCEOSalary
+        CONCAT(P.FirstName, ' ', P.LastName) AS ExecutiveName,
+        S.BaseSalary AS ExecutiveSalary,
+        R.[Name] AS RoleName
     FROM [Org].[Employee] E
-        INNER JOIN [Org].[Person] P ON P.PersonID = E.PersonID
+        INNER JOIN [Org].[Person] P ON E.PersonID = P.PersonID
         INNER JOIN [Org].[Salary] S ON S.SalaryID = E.EmployeeID
         INNER JOIN [Org].[DepEmpRole] DER ON DER.EmployeeID = E.EmployeeID
         INNER JOIN [Org].[Role] R ON R.RoleID = DER.RoleID
         INNER JOIN [Org].[Department] D ON D.DepartmentID = DER.DepartmentID
         INNER JOIN [Org].[Company] C ON C.CompanyID = D.CompanyID
         INNER JOIN [Org].[EmployeeType] ET ON ET.EmployeeTypeID = E.EmployeeTypeID
-    WHERE R.[Name] = 'CEO'
-    GROUP BY C.[Name];
+    WHERE R.[Name] IN ('CEO', 'CFO', 'CTO', 'CHRO')
+    ORDER BY S.BaseSalary DESC;
 END
 GO
--- src/company/sql/Procedures/Reports/Report_HighestPaidExecutive_Aggregated.sql
+++ src/company/sql/Procedures/Reports/Report_HighestPaidExecutive_Aggregated.sql
@@ -1,19 +1,19 @@
-CREATE PROCEDURE [Org].[sp_Report_HighestPaidCEO]
+CREATE PROCEDURE [Org].[sp_Report_HighestPaidExecutive_Aggregated]
 AS
 BEGIN
-    SELECT TOP(1)
+    SELECT
         C.[Name] AS CompanyName,
-        CONCAT(P.FirstName, ' ', P.LastName) AS CEOName,
-        S.BaseSalary AS CEOSalary
+        COUNT(*) AS ExecutiveCount,
+        MAX(S.BaseSalary) AS HighestExecutiveSalary
     FROM [Org].[Employee] E
-        INNER JOIN [Org].[Person] P ON E.PersonID = P.PersonID
+        INNER JOIN [Org].[Person] P ON P.PersonID = E.PersonID
         INNER JOIN [Org].[Salary] S ON S.SalaryID = E.EmployeeID
         INNER JOIN [Org].[DepEmpRole] DER ON DER.EmployeeID = E.EmployeeID
         INNER JOIN [Org].[Role] R ON R.RoleID = DER.RoleID
         INNER JOIN [Org].[Department] D ON D.DepartmentID = DER.DepartmentID
         INNER JOIN [Org].[Company] C ON C.CompanyID = D.CompanyID
         INNER JOIN [Org].[EmployeeType] ET ON ET.EmployeeTypeID = E.EmployeeTypeID
-    WHERE R.[Name] = 'CEO'
-    ORDER BY S.BaseSalary DESC;
+    WHERE R.[Name] IN ('CEO', 'CFO', 'CTO', 'CHRO')
+    GROUP BY C.[Name];
 END
 GO
